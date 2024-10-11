fdd44bbcf7920eb2294e7334808ba21d33dacec3
rosenclever <rosenclever@gmail.com>
2024-10-11 09:18:01 -0300

    nuget and expocanpContext configured

---
+++ ControleRegistroAtividades/Data/ExpocanpContext.cs
@@ -0,0 +1,13 @@
+﻿using ControleRegistroAtividades.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleRegistroAtividades.Data
+{
+    public class ExpocanpContext : DbContext
+    {
+        public ExpocanpContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Sala> Salas { get; set; }
+    }
+}
---
+++ ControleRegistroAtividades/Models/Sala.cs
@@ -0,0 +1,12 @@
+﻿namespace ControleRegistroAtividades.Models
+{
+    public class Sala
+    {
+        private string _identificacao;
+        private int _capacidade;
+        public string Identificacao { get { return _identificacao; }
+            set { _identificacao = value; } }
+        public int Capacidade { get { return _capacidade; }
+            set { _capacidade = value; } }
+    }
+}
