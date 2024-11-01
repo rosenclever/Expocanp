364c2af91b07ca88c53b371cb62c97bf9b40ddde
rosenclever <rosenclever@gmail.com>
2024-11-01 08:01:26 -0300

    configuracoes do nuget, string de conexao e context

--- ControleRegistroAtividades/Program.cs
+++ ControleRegistroAtividades/Program.cs
@@ -1,7 +1,12 @@
+using ControleRegistroAtividades.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+var connectionString = builder.Configuration.GetConnectionString("ExpocanpConnection");
+builder.Services.AddDbContext<ExpocanpContext>(options => options.UseSqlServer(connectionString));
 
 var app = builder.Build();
 
